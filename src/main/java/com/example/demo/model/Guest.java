45d3174a581739d1a49c8c88b3acc336bbecdea1
bala-amypo <bala@amypo.in>
2025-12-18 06:31:32 +0000

    User commit

--- src/main/java/com/example/demo/model/Guest.java
+++ src/main/java/com/example/demo/model/Guest.java
@@ -4,7 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
-
+import java.time.LocalDateTime;
 @Entity
 public class StudentEntity {
     @Id
@@ -18,4 +18,23 @@ public class StudentEntity {
     private boolean active = true;
     private String role;
     private LocalDateTime createdAt;
+
+    public void setEmail(String email){
+        this.email=email;
+    }
+    public void setFullName(String fullName){
+        this.fullName=fullName;
+    }
+    public void setPhoneNumber(String phoneNumber){
+        this.phoneNumber=phoneNumber;
+    }
+    public void setVerified(boolean verified){
+        this.verified=verified;
+    }
+    public void setActuiv(String email){
+        this.email=email;
+    }
+    public void setEmail(String email){
+        this.email=email;
+    }
 }
