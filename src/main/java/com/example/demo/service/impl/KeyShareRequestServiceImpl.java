fc2974bed474f9f033feefeea5a4fa37e4116c37
bala-amypo <bala@amypo.in>
2025-12-22 05:56:27 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/KeyShareRequestServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/KeyShareRequestServiceImpl.java
@@ -1,6 +1,10 @@
 package com.example.demo.service.impl;
 
+import com.example.demo.model.DigitalKey;
+import com.example.demo.model.Guest;
 import com.example.demo.model.KeyShareRequest;
+import com.example.demo.repository.DigitalKeyRepository;
+import com.example.demo.repository.GuestRepository;
 import com.example.demo.repository.KeyShareRequestRepository;
 import com.example.demo.service.KeyShareRequestService;
 import com.example.demo.exception.ResourceNotFoundException;
@@ -12,20 +16,42 @@ import java.util.List;
 public class KeyShareRequestServiceImpl implements KeyShareRequestService {
 
     private final KeyShareRequestRepository repo;
+    private final DigitalKeyRepository digitalKeyRepo;
+    private final GuestRepository guestRepo;
 
-    public KeyShareRequestServiceImpl(KeyShareRequestRepository repo) {
+    public KeyShareRequestServiceImpl(
+            KeyShareRequestRepository repo,
+            DigitalKeyRepository digitalKeyRepo,
+            GuestRepository guestRepo
+    ) {
         this.repo = repo;
+        this.digitalKeyRepo = digitalKeyRepo;
+        this.guestRepo = guestRepo;
     }
 
     @Override
     public KeyShareRequest createShareRequest(KeyShareRequest request) {
+
         if (request.getShareEnd().isBefore(request.getShareStart())) {
-            throw new IllegalArgumentException("share end");
+            throw new IllegalArgumentException("Share end must be after start");
         }
+
         if (request.getSharedBy().getId().equals(request.getSharedWith().getId())) {
-            throw new IllegalArgumentException("sharedBy and sharedWith");
+            throw new IllegalArgumentException("Cannot share key with same guest");
         }
+        DigitalKey key = digitalKeyRepo.findById(request.getDigitalKey().getId())
+                .orElseThrow(() -> new ResourceNotFoundException("Digital key not found"));
+
+        Guest sharedBy = guestRepo.findById(request.getSharedBy().getId())
+                .orElseThrow(() -> new ResourceNotFoundException("SharedBy guest not found"));
+
+        Guest sharedWith = guestRepo.findById(request.getSharedWith().getId())
+                .orElseThrow(() -> new ResourceNotFoundException("SharedWith guest not found"));
+        request.setDigitalKey(key);
+        request.setSharedBy(sharedBy);
+        request.setSharedWith(sharedWith);
         request.setStatus("PENDING");
+
         return repo.save(request);
     }
 
