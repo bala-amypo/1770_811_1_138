c8c7493eea4262a3eeacc9804b6ebc368882bf14
bala-amypo <bala@amypo.in>
2025-12-25 06:44:36 +0000

    User commit

--- src/main/java/com/example/demo/repository/GuestRepository.java
+++ src/main/java/com/example/demo/repository/GuestRepository.java
@@ -1,13 +1,1 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.model.Guest;
-import java.util.Optional;
-
-public interface GuestRepository extends JpaRepository<Guest, Long> {
-
-    Optional<Guest> findByEmail(String email);
-
-    // REQUIRED BY TESTS
-    boolean existsByEmail(String email);
-}
+package com.example.demo.repository; import org.springframework.data.jpa.repository.JpaRepository; import com.example.demo.model.Guest; import java.util.Optional; public interface GuestRepository extends JpaRepository<Guest, Long> { Optional<Guest> findByEmail(String email); }
